b6934447b8c8803a474d674de78d078c432df341
JhomAlmeida <jhohalmeida@gmail.com>
2026-01-26 19:31:05 -0300

    Create App.js

---
+++ src/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Importando as páginas da pasta correta
+import Home from './pages/Home';      
+import Aula01 from './pages/Aula01';  
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aula-01" element={<Aula01 />} />
+      </Routes>
+    </Router>
+  );
+}
